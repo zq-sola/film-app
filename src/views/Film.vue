a2acea6a6bb2bcfd27ed5c3e897da5382fa962a3
sola <d2503751@qq.com>
2021-06-18 01:23:53 +0800

    1

--- src/views/Film.vue
+++ src/views/Film.vue
@@ -2,7 +2,7 @@
 <template>
   <div>
     <filmTab></filmTab>
-    
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum recusandae quasi et sint ratione animi necessitatibus eaque molestias dicta! At quis esse, voluptatum deserunt similique labore consectetur atque sequi excepturi.
   </div>
   
 </template>
